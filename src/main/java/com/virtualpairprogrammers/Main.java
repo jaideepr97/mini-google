fe961dba3bc612a205ea4802a992c35e0dd7d1a0
Aayush Gupta <aayushgupta@Aayushs-MacBook-Pro.local>
2019-10-06 21:30:48 -0400

    Added functionality for Inverted index.

--- src/main/java/com/virtualpairprogrammers/Main.java
+++ src/main/java/com/virtualpairprogrammers/Main.java
@@ -1,6 +1,7 @@
 package com.virtualpairprogrammers;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.*;
 import java.util.List;
 
 import org.apache.spark.SparkConf;
@@ -8,40 +9,63 @@ import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 
+import org.apache.spark.api.java.function.FlatMapFunction;
 import scala.Tuple2;
+import scala.util.parsing.combinator.testing.Str;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		SparkConf conf = new SparkConf().setAppName("FirstApp").setMaster("local[*]");
+		String path = "hdfs://localhost:8020/docfiles/";
 		JavaSparkContext sc = new JavaSparkContext(conf);
-		
-//		JavaRDD<String> inputdocs = sc.textFile("hdfs://localhost:9000/user/Project1_data/*");
-//		JavaPairRDD<String, Integer> words = inputdocs.flatMap(line -> Arrays.asList(line.split(" ")).iterator()).mapToPair(word -> new Tuple2<String, Integer>(word, 1))
-//        .reduceByKey((x, y) ->  x +  y)
-//        .sortByKey();
-//		words.foreach(data -> {
-//	        System.out.println(data._1()+"-"+data._2());
-//	    });
-		JavaPairRDD<String, String> inputdocs = sc.wholeTextFiles("hdfs://localhost:9000/user/Project1_data/*");
-
-		JavaPairRDD<List<String>, String> sentences = inputdocs.mapToPair(data -> new Tuple2< List<String>, String>(Arrays.asList(data._2().split(" ")), data._1()));
-//		JavaPairRDD<String, String> words = sentences.mapToPair(data ->  data._1().forEach(item -> {new Tuple2<String, String> (item, data._2()); } )); 
-		List<Tuple2<String, String>> temp = new ArrayList<>();
-		sentences.foreach(sentence -> {
-			List<String> sent = sentence._1();
-			
-			sent.forEach(word -> {
-				
-				temp.add(new Tuple2<String, String> (word.toLowerCase(), sentence._2()));
-			});
+		//Get all the input files as (docPath, content)
+		JavaPairRDD<String, String> inputdocs = sc.wholeTextFiles(path+"*");
+		//Convert it to an rdd as (docPath, List of words)
+		JavaPairRDD<String, List<String>> docToWords = inputdocs.mapToPair(data -> new Tuple2<>(data._1.substring(path.length()), Arrays.asList(data._2().split(" "))));
+		//Reverse it as (List of words, docPath)
+		JavaPairRDD<List<String>, String> wordsToDoc = docToWords.mapToPair(data -> new Tuple2<>(data._2(), data._1()));
+		//Flatten it to (word, docPath)
+		JavaPairRDD<String, String> flatWordsToDoc = wordsToDoc.flatMapToPair(data ->
+		{
+			List<String> _words = data._1;
+			String _doc = data._2;
+			List<Tuple2<String, String>> _result = new ArrayList();
+			for(String _w : _words)
+			{
+				_result.add(new Tuple2<>(_w, _doc));
+			}
+			return _result.iterator();
 		});
-		JavaRDD<Tuple2<String, String>> rdd = sc.parallelize(temp);
-		JavaPairRDD<String, String> words = JavaPairRDD.fromJavaRDD(rdd);
-//		JavaPairRDD<String, String> words = sentences.
-		words.foreach(data -> {
-	        System.out.println(data._1()+"-"+data._2());
-	    });
+		//Optional - Remove special characters from flatWordsToDoc
+		flatWordsToDoc = flatWordsToDoc.mapToPair(data -> new Tuple2<>(data._1.replaceAll("[ ](?=[ ])|[^-_A-Za-z0-9 ]+", ""),data._2()));
+		//Creating a new rdd as ((word, doc), 1) - starting the process of calculating wordcount
+		JavaPairRDD<Tuple2<String, String>, Integer> wordDocToOne = flatWordsToDoc.mapToPair(data -> new Tuple2<>(new Tuple2<>(data._1, data._2),1));
+		//Grouping all the (word, doc) pairs and summing the word count
+		JavaPairRDD<Tuple2<String, String>, Integer> wordDocToCount = wordDocToOne.reduceByKey((x,y) -> x+y);
+		//Converting the tuple ((word,docPath), count) to (word, (docPath,count))
+		JavaPairRDD<String, Tuple2<String, Integer>> wordToDocCount = wordDocToCount.mapToPair(data -> new Tuple2<>(data._1._1, new Tuple2<>(data._1._2, data._2)));
+		// Grouping by words
+		JavaPairRDD<String, Iterable<Tuple2<String, Integer>>> wordToDocCountGrouped = wordToDocCount.groupByKey();
+		List<Tuple2<String, Iterable<Tuple2<String, Integer>>>>result = wordToDocCountGrouped.take(10);
+		for(Tuple2<String, Iterable<Tuple2<String, Integer>>> r : result)
+		{
+			System.out.println("Word:" + r._1);
+			System.out.println("Docs | Counts");
+			for(Tuple2<String, Integer> d : r._2)
+				System.out.println(d._1 + " " + d._2);
+			System.out.println(".........................................");
+		}
+		//JavaPairRDD<List<String>, String> sentences = inputdocs.mapToPair(data -> new Tuple2< List<String>, String>(Arrays.asList(data._2().split(" ")), data._1()));
+		/*
+		List<Tuple2<String, String>> result = inputdocs.collect();
+		for (Tuple2<String, String> res : result) {
+			// Note that the paths from `wholeTextFiles` are in URI format on Windows,
+			// for example, file:/C:/a/b/c.
+			System.out.println(res._1 + "-" + res._2);
+		}
+		*/
 	}
 
 }
